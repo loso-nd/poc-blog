48850424bac9106d379df1b58bbe55430a666b1e
loso-nd <losond8@gmail.com>
2025-01-14 18:54:43 -0600

    Refactor upvotes, comments endpoint

--- my-poc-blog-backend/src/server.js
+++ my-poc-blog-backend/src/server.js
@@ -1,26 +1,13 @@
 import express from "express";
-import { MongoClient } from 'mongodb';
+import { db, connectToDb } from "./db.js";
 
 const app = express();
 app.use(express.json());
 
-/**
- * In lieu of db in memory we are to ging to connect to db and make queries to the db we created ( react-poc-blog)
- * Create a new endpoint to allow our cient side to actually load the information for a give article
- * We need to know how my upvotes & comments an article has so we can display them
- */
+
 app.get('/api/articles/:name', async (req, res) => {
-    // get value of the url param
-    const { name } = req.params;
+  const { name } = req.params
 
-    // connecting to the mongodb instance we have running
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
-
-    // Retrieve the specific db the we created and reference to it
-    const db = client.db('react-poc-blog');
-
-    // Passing a collection of articles and findOne({}) allows us to find a single document inside the mongoDb collection
     const article = await db.collection('articles').findOne({ name });
 
     // Check to see if the article exist and if it does we will retrieve the aricle else 404
@@ -32,46 +19,57 @@ app.get('/api/articles/:name', async (req, res) => {
 });
 
 //PUT
-app.put('/api/articles/:name/upvote', (req, res) => {
-// define a fake database to keep track of all the upvotes each article has to get the logic right
-// in each of our handlers
+app.put('/api/articles/:name/upvote', async (req, res) => {
 
-    // What article do we want to upvote?
-    // This gives us the current value at the name segment in the request we just received 
     const { name } = req.params
     
-    // Find the cooresponding article w/ that name
-    const article = articleInfo.find((a) => a.name === name);
+    /**
+     * make a query to MongoDb to the collection of articles
+     * First arg in the db.collection call is the type of query call, 
+     * Second arg contain the changes that we specify MongoDB to make
+     * 
+     * Goal:We want to update an article whose name property is equal to the URL param,
+     */
+    // 
+    await db.collection('articles').updateOne({ name }, {
+        $inc: { upvotes: 1 }
+    });
+
+    // Passing a collection of articles and findOne({}) allows us to find a single document inside the mongoDb collection
+    const article = await db.collection('articles').findOne({ name });
 
     // check if article exist
     if (article) {
-        article.upvotes += 1;
-        res.send(`The ${name} article now has ${article.upvotes} upvotes!! Read Now!`)
+        const total = `${article.upvotes} <= 1` ? `${article.upvotes}  upvotes` : `${article.upvotes} upvote!!`
+        res.send(`The ${name} article now has ${total}... Read Now!`)
     } else {
         res.send(`That article doesn\'t exist`);
     }
 
 })
 
-app.post('/api/articles/:name/comments', (req, res) => {
-    //1st we want to determine which format the comments are going to be specified when 
-    //they are sent to the server request
+app.post('/api/articles/:name/comments', async (req, res) => {
     const { name } = req.params;
     const { postedBy, message } = req.body
 
-    const article = articleInfo.find((a) => a.name === name);
+    //Make a query to mongoDb 
+    await db.collection('articles').updateOne({ name }, {
+        $push: { comments: { postedBy, message } },
+    })
+    const article = await db.collection("articles").findOne({ name });
 
-        if (article) {
-        article.comments.push({postedBy, message})
-        // res.send(`The ${name} article now has a new entry written by ${postedBy} with the comment "${message}"`)
+    if (article) {
         res.send(article.comments)
-    } else {
+    }
+     else {
         res.send(`That article doesn\'t exist`);
     }
 
 })
 
-
-app.listen(8000, () => {
-    console.log("Server is listening on port 8000")
-});
+connectToDb(() => {
+    console.log("Successfully connected to the database")
+    app.listen(8000, () => {
+        console.log("Server is listening on port 8000")
+    });
+})
